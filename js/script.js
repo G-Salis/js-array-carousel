9c7740b88275327f13a8039ad37012d53edb37f5
G-Salis <giuseppesalislavoro@gmail.com>
2021-10-27 12:22:20 +0200

    aggiunti array

--- js/script.js
+++ js/script.js
@@ -7,3 +7,31 @@ Al click dell’utente sulle frecce verso l’alto o verso il basso, l’immagin
  - titolo e
  - testo.
 Allo stesso tempo nelle miniature l’immagine attiva dovrà apparire in evidenza rispetto alle altre. */
+
+const items = [
+  'img/01.jpg',
+  'img/02.jpg',
+  'img/03.jpg',
+  'img/04.jpg',
+  'img/05.jpg'
+];
+
+const title = [
+  'Svezia',
+  'Svizzera',
+  'Gran Bretagna',
+  'Germania',
+  'Paradise'
+]
+
+const text = [
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+
+  'Lorem ipsum',
+
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+  
+  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
